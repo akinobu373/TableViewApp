9dd8722b3a4f9c3881a9a7177d3172eba2305044
akinobu <siguiente.football.373@gmail.com>
2021-10-31 17:54:16 +0900

    タスク追加まで出来た！

---
+++ TableViewApp/AddListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddListViewController.swift
+//  TableViewApp
+//
+//  Created by 美並　明伸 on 2021/10/31.
+//
+
+import UIKit
+
+class AddListViewController: UIViewController {
+    
+    @IBOutlet weak var addTextField: UITextField!
+    var taskArray: [String] = [] //追加したリストを格納する配列
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let userDefaults = UserDefaults.standard
+        if userDefaults.object(forKey: "add") != nil{
+        taskArray = userDefaults.object(forKey: "add") as! [String]
+        // Do any additional setup after loading the view.
+        }
+    }
+    
+    @IBAction func addTask(_ sender: Any) {
+    
+        let userDefaults = UserDefaults.standard
+        taskArray.append(addTextField.text!)
+        
+
+        userDefaults.set(taskArray, forKey: "add")
+    
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
